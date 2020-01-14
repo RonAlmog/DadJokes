ef86d93ca9d023251c5efb08835170abd4d402de
Ron Almog <ron8132@gmail.com>
2020-01-14 15:03:31 -0500

    basic nuxt setup, 3 pages

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>About Dad Jokes</h1>
+      <p>This app will display too many corny jokes for your enjoyment</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About the app',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place to find dad jokes'
+                }
+                
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
