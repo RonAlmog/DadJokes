82f4c840d26560225e19cfa4cb8c56d47e60df5e
Ron Almog <ron8132@gmail.com>
2020-01-14 20:05:20 -0500

    single item, navigation, params

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <nuxt-link to="/jokes">Back to Jokes</nuxt-link>
+       <h3>Single Joke</h3>      
+    <h2>{{joke}}</h2>
+    <hr>
+    <small>Joke ID: {{$route.params.id}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      joke: []
+    }
+  },
+  async created() {
+    try {
+      const config = {
+        headers: { Accept: 'application/json' }
+      }
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+      this.joke = res.data.joke;
+
+    } catch (error) {}
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>Jokes</div>
+  <div>
+      <h1>Joke</h1>
+      <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" >
+
+      </Joke>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Joke from '@/components/Joke';
 export default {
+  data() {
+    return {
+      jokes: []
+    }
+  },
+  components: {
+      Joke
+  },
+  async created() {
+    const config = {
+      headers: { Accept: 'application/json' }
+    }
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config);
+      this.jokes = res.data.results;
 
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  head() {
+    return {
+      title: 'Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place to find dad jokes'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
